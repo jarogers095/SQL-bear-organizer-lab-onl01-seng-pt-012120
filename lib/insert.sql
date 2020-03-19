27523b1282aca1d20dc3c41289f4d2dfacd3f3e8
James Rogers <jarogers095@gmail.com>
2020-03-19 00:32:43 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,135 @@
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Mr. Chocolate",
+  20,
+  "Male",
+  "Brown",
+  "Smooth",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Rowdy",
+  16,
+  "Male",
+  "Red",
+  "Vivacious",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Tabitha",
+  5,
+  "Female",
+  "Pink",
+  "Cute",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Sergeant Brown",
+  72,
+  "Male",
+  "Brown",
+  "Stern",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Melissa",
+  35,
+  "Female",
+  "Blue",
+  "Caring",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Grinch",
+  156,
+  "Male",
+  "Green",
+  "Scheming",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  "Wendy",
+  17,
+  "Female",
+  "White",
+  "Jaded",
+  1
+);
+
+INSERT INTO bears (
+  name, 
+  age, 
+  gender, 
+  color, 
+  temperament, 
+  alive
+)
+VALUES (
+  NULL,
+  52,
+  "Male",
+  "Grey",
+  "Murderous",
+  0
+);
